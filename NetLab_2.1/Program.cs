98ac64131f5149bf3602623a821e44cc5a1e84f6
Darkest-White <darkest.white.fox@gmail.com>
2023-09-28 00:05:41 +0300

    Add project files.

---
+++ NetLab_2.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace NetLab2_1
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            //Form myForm = new Form();
+            //myForm.Show();
+        }
+    }
+}
